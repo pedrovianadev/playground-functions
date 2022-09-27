82487ad8961f513002d24142a76eaac7b3d59813
Pedro Viana <pedroviana.developer13@gmail.com>
2022-09-27 18:29:35 -0300

    desafio 7, eu errei o inferno da chave pq sem querer apagou

--- src/challenges.js
+++ src/challenges.js
@@ -44,10 +44,17 @@ function highestCount(array) {
   return count;
 }
 
-
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+
+
+  if((Math.abs(mouse - cat1)) < (Math.abs(mouse - cat2))){
+    return 'cat1';
+  } else if((Math.abs(mouse - cat1)) === (Math.abs(mouse - cat2))){
+    return 'os gatos trombam e o rato foge';
+  } else {
+    return 'cat2';
+}
 }
 
 // Desafio 8
